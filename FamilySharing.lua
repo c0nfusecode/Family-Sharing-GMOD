c7aee592a31cfea90bff05eb0eab4708982af7c9
c0nfusecode <55307836+c0nfusecode@users.noreply.github.com>
2023-03-11 23:31:58 +0600

    Update FamilySharing.lua

--- FamilySharing.lua
+++ FamilySharing.lua
@@ -1,3 +1,4 @@
 hook.Add("PlayerAuthed", "LibFuse:FamilySharing", function(ply)
+    if ply:IsBot() then return end
     if ply:SteamID64() ~= ply:OwnerSteamID64() then ply:Kick "Family Sharing is not welcomed on this server" end
 end) -- а пахне як, без ваших steamapi!
